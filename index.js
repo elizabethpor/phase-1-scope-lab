46330072be507f43f6cf4a1bce477f09e3ca5224
Elizabeth <elizabeth.por@gmail.com>
2022-02-03 10:57:03 -0600

    commit tree 3f1437e4b6840a9f6c97de55e80908046ab9ed8e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName = "bob";
+//console.log("num3: ", customerName);
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+//    console.log("num6: ", customerName);
+}
+//console.log("num9: ", customerName);
+
+function setBestCustomer() {
+    return bestCustomer = "not bob";
+}
